fe80683c2f1fa597312192096a6455fc2276dc70
Veronika Cotkova <v.cotkova@gmail.com>
2019-10-11 16:12:41 +0200

    Adding task 12 and 13 from string playground

---
+++ 03/task12_string_playground_02.py
@@ -0,0 +1,8 @@
+letter = 'x'
+magnitude = 5
+
+for row in range(magnitude):
+    for column in range(magnitude):
+        print(letter, end=' ')
+    print(end='\n')
+
---
+++ 03/task13_string_playground_03.py
@@ -0,0 +1,6 @@
+magnitude = 5
+
+for row in range(magnitude):
+    for column in range(magnitude):
+        print(row * column, end=' ')
+    print(end='\n')
