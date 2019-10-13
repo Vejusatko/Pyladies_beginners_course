0c3797cf6468575528fbc97e5a4c89906589c73d
Veronika Cotkova <v.cotkova@gmail.com>
2019-10-13 16:17:51 +0200

    Hangman coding continues

--- 03/task21_hangman.py
+++ 03/task21_hangman.py
@@ -18,6 +18,7 @@ Pokud je počet neúspěšných pokusů 9 (nebo víc), hráč prohrál. Dej mu t
 def return_state(state):
     for i in state:
         print(i, end=' ')
+    print()
 
 def hang(level):
     if level == 0:
@@ -115,7 +116,45 @@ def hang(level):
         ~~~~~~~
         """
 
-word = 'travnik'
-state = '_' * len(word)
+def replace_character(slovo, stav, character):
+    index = slovo.index(character)
+    while stav[index] == '_':
+        stav.replace(stav[index], character)
+        index = slovo.index(character, index + 1)
 
-return_state(state)
+
+.
+..
+
+secret_word = 'travnik'
+word_lenght = len(secret_word)
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+guessed_characters = []
+
+def guessing():
+    wrong_answer = 0
+    state = '_' * word_lenght
+    while wrong_answer <= 9:
+        guess = input("Guess your character, puny human: ").lower()
+        if guess not in alphabet:
+            guess = input("Incorrect input! Enter a letter from a-z alphabet only: ").lower()
+        elif guess in guessed_characters:
+            guess = input("You already guessed that character, but I will give you another try: ").lower()
+        else:
+            guessed_characters.append(guess)
+            if guess in secret_word:
+                print("You got that right! Good for you!")
+                for i in range(0,word_lenght):
+                    if secret_word[i] == guess:
+                        state = state.replace(state[i],guess)
+                        return_state(state)
+            else:
+                wrong_answer += 1
+                if wrong_answer == 9:
+                    print("You looooosed!")
+                    hang(9)
+                else:
+                    print("Wrong guess! Try again!")
+                    hang(wrong_answer)
+
+guessing()
